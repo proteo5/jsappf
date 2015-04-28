1d37004b88f14a4653eca0039e76a772593f575a
Alfredo Pinto <alfredo.pinto@freewayinsurance.com>
2015-04-28 15:56:26 -0700

    Enhance the sample

--- project/app/index.js
+++ project/app/index.js
@@ -5,7 +5,7 @@
     description: "This is our default page",
     elements: {},
     events: {
-        start: function(){
+        start: function () {
             var elements = app.modules.index.elements;
             var events = app.modules.index.events;
             app.loadView('index', 'page');
@@ -18,19 +18,31 @@
                 var setLanguage = app.localization.get() == "en-US" ? "es-MX" : "en-US";
                 app.localization.set(setLanguage, app.modules.index.resources, app.modules.index.elements);
             }
+        },
+        buttonComand: {
+            click: function () {
+                app.modules.index.code.actionX();
+            }
         }
     },
     code: {
-       
+        actionX: function () {
+            var el = app.modules.index.elements;
+            el.message.html(el.textField.val());
+        }
     },
     resources: {
-        header:{
+        header: {
             "en-US": "Home",
             "es-MX": "Inicio"
         },
         lnkLocalization: {
-            "en-US": "Español (es-MX)",
-            "es-MX": "English (en-US)"
+            "en-US": "Change to: Español (es-MX)",
+            "es-MX": "Cambiar a: English (en-US)"
+        },
+        buttonComand: {
+            "en-US": "Ok",
+            "es-MX": "Esta bien"
         }
     }
 }
